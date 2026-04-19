1d8ad4f546acdbe05ac26fd585280c52a7173567
Auriane <auriane.b8@gmail.com>
2026-04-19 23:17:36 +0300

    daily week2 day3

---
+++ Week02/Day3/Daily/daily.py
@@ -0,0 +1,34 @@
+#%%
+# Here is a python code that generates a list of 20000 random numbers,
+#  called list_of_numbers, and a target number.
+
+# import random
+
+# list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
+
+# target_number   = 3728
+
+# Copy this code, and create a program that finds,
+# within list_of_numbers all the pairs of number that sum to the target number
+
+# For example
+# 1000 and 2728 sums to the target_number 3728
+# 1864 and 1864 sums to the target_number 3728
+
+import random
+
+list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
+
+target_number = 3728
+
+seen = set()
+
+for number in list_of_numbers:
+    other_number = target_number - number
+    
+    if other_number in seen:
+        print(f"{number} and {other_number} sums to {target_number}")
+    
+    seen.add(number)
+
+# %%
