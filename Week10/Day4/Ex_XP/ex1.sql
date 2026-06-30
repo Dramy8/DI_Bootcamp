fa5fa6fde577c23d3b323ac1d856c7670e8b16be
Auriane <auriane.b8@gmail.com>
2026-06-30 12:23:24 +0300

    day4w10

---
+++ Week10/Day4/Ex_XP/ex1.sql
@@ -0,0 +1,26 @@
+-- EX1 
+-- Use SQL to get the following from the database:
+-- All items, ordered by price (lowest to highest).
+-- Items with a price above 80 (80 included), ordered by price (highest to lowest).
+-- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
+-- All last names (no other columns!), in reverse alphabetical order (Z-A)
+
+
+SELECT *
+FROM items
+ORDER BY item_price ASC;
+
+SELECT *
+FROM items
+WHERE item_price >= 80
+ORDER BY item_price DESC;
+
+SELECT first_name, last_name
+FROM customers
+ORDER BY first_name
+LIMIT 3;
+
+SELECT last_name
+FROM customers
+ORDER BY last_name DESC;
+
